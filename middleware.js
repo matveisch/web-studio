b532b91328cab177135c1d39b172a49e62852a49
matveisch <matvesch@protonmail.com>
2025-05-16 13:55:00 +0300

    refactor: update middleware config matcher to exclude robots.txt and images

--- middleware.js
+++ middleware.js
@@ -30,5 +30,7 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|robots.txt|images|api).*)",
+  ],
 };
